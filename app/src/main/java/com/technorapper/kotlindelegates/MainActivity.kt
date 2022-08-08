6b112c9c93121730f1676d16e88c42e8f29ecf22
Haneet Singh Chhabra <haneet555@gmail.com>
2022-08-09 00:29:26 +0530

    MyLazy handler

--- app/src/main/java/com/technorapper/kotlindelegates/MainActivity.kt
+++ app/src/main/java/com/technorapper/kotlindelegates/MainActivity.kt
@@ -6,11 +6,21 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity(), AnalyticLogger by AnalyticLoggerImp(),
     DeeplinkHandler by DeeplinkHandlerImp() {
+    private val obj by lazy {
+        println("Hello")
+        3
+    }
+    private val obj1 by MyLazy {
+        println("Hello")
+        4
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         registerLifecycleOwner(this)
-
+        println(obj)
+        println(obj1)
     }
 
     override fun onNewIntent(intent: Intent?) {
---
+++ app/src/main/java/com/technorapper/kotlindelegates/MyLazy.kt
@@ -0,0 +1,15 @@
+package com.technorapper.kotlindelegates
+
+import kotlin.reflect.KProperty
+
+class MyLazy<out T : Any>(
+    private val initialize: () -> T
+) {
+    private var value: T? = null
+    operator fun getValue(thisRef: Any?, property: KProperty<*>): T {
+        return if (value == null) {
+            value = initialize()
+            value!!
+        } else value!!
+    }
+}
